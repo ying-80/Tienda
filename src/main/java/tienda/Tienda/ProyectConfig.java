f310f33cc78c13790afcb3af42799abeb4febc77
ying-80 <ying_07@outlook.com>
2023-06-15 21:09:32 -0600

    semana 5

---
+++ src/main/java/tienda/Tienda/ProyectConfig.java
@@ -0,0 +1,93 @@
+package tienda.Tienda;
+//notaciones
+
+import java.util.Locale;
+
+ 
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+ 
+
+@Configuration
+public class ProyectConfig implements WebMvcConfigurer {
+
+ 
+
+    @Bean
+    public LocaleResolver localeResolver() {
+
+ 
+
+        var slr = new SessionLocaleResolver();
+
+ 
+
+        slr.setDefaultLocale(Locale.getDefault());
+
+ 
+
+        slr.setLocaleAttributeName(
+                "session.current.locale");
+
+ 
+
+        slr.setTimeZoneAttributeName(
+                "session.current.timezone");
+
+ 
+
+        return slr;
+    }
+
+ 
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+
+ 
+
+        var lci = new LocaleChangeInterceptor();
+
+ 
+
+        lci.setParamName("lang");
+
+ 
+
+        return lci;
+    }
+
+ 
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registro) {
+        registro.addInterceptor(localeChangeInterceptor());
+    }
+
+
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource
+                = new ResourceBundleMessageSource();
+
+ 
+
+        messageSource.setBasenames("messages");
+
+ 
+
+        messageSource.setDefaultEncoding("UTF-8");
+
+ 
+
+        return messageSource;
+    }
+}
