4e967686a670a794aab1fbcbe6eab36649347224
ying-80 <ying_07@outlook.com>
2023-07-05 23:05:33 -0600

    semana 8

--- src/main/java/tienda/Tienda/service/CategoriaService.java
+++ src/main/java/tienda/Tienda/service/CategoriaService.java
@@ -4,5 +4,18 @@ import java.util.List;
 import tienda.Tienda.domain.Categoria;
 
 public interface CategoriaService {
+    //CRUD CREATE DELETE READ DELETE
+
+    // Se obtiene un listado de categorias en un List
     public List<Categoria> getCategorias(boolean activos);
+    
+    // Se obtiene un Categoria, a partir del id de un categoria
+    public Categoria getCategoria(Categoria categoria);
+
+    // Se inserta un nuevo categoria si el id del categoria esta vacío
+    // Se actualiza un categoria si el id del categoria NO esta vacío
+    public void save(Categoria categoria);
+
+    // Se elimina el categoria que tiene el id pasado por parámetro
+    public void delete(Categoria categoria);
 }
--- src/main/java/tienda/Tienda/service/impl/CategoriaServiceIpml.java
+++ src/main/java/tienda/Tienda/service/impl/CategoriaServiceIpml.java
@@ -3,6 +3,7 @@ package tienda.Tienda.service.impl;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import tienda.Tienda.dao.CategoriaDao;
 import tienda.Tienda.domain.Categoria;
 import tienda.Tienda.service.CategoriaService;
@@ -23,6 +24,24 @@ public class CategoriaServiceIpml implements CategoriaService {
         }
 
         return lista;
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public Categoria getCategoria(Categoria categoria) {
+        return categoriaDao.findById(categoria.getIdCategoria()).orElse(null);
+    }
+
+    @Override
+    @Transactional
+    public void save(Categoria categoria) {
+        categoriaDao.save(categoria);
+    }
+
+    @Override
+    @Transactional
+    public void delete(Categoria categoria) {
+        categoriaDao.delete(categoria);
     }
 
 }
