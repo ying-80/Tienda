a30b72aa1f39f79919f26bfbf23ba4e05b605663
ying-80 <ying_07@outlook.com>
2023-08-11 01:23:45 -0600

    commit5

--- tienda_1/src/main/java/tienda/Tienda/controller/PruebasController.java
+++ tienda_1/src/main/java/tienda/Tienda/controller/PruebasController.java
@@ -13,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
+import tienda.Tienda.domain.Categoria;
 import tienda.Tienda.domain.Producto;
 import tienda.Tienda.service.CategoriaService;
 import tienda.Tienda.service.ProductoService;
@@ -20,8 +21,8 @@ import tienda.Tienda.service.FirebaseStorageService;
 
 @Controller
 @Slf4j
-@RequestMapping("/producto")
-public class ProductoController {
+@RequestMapping("/pruebas")
+public class PruebasController {
 
     @Autowired
     private ProductoService productoService;
@@ -32,46 +33,24 @@ public class ProductoController {
     @GetMapping("/listado")
     public String inicio(Model model){
         var productos = productoService.getProductos(false);
+        var categorias = categoriaService.getCategorias(false);
+        model.addAttribute("productos", productos);
+        model.addAttribute("totalProductos", productos.size());
+        model.addAttribute("categorias", categorias);
+        return "/pruebas/listado";  
+    }
+    
+    
+        @GetMapping("/listado/{idCategoria}")
+    public String inicio(Model model, Categoria categoria){
+        var productos = categoriaService.getCategoria(categoria).getProductos();
         var categorias = categoriaService.getCategorias(true);
         model.addAttribute("productos", productos);
         model.addAttribute("totalProductos", productos.size());
         model.addAttribute("categorias", categorias);
-        return "/producto/listado";
-    
-    }
-    
-     @GetMapping("/nuevo")
-    public String productoNuevo(Producto producto) {
-        return "/producto/modifica";
-    }
-    
-    @PostMapping("/guardar")
-    public String productoGuardar(Producto producto,
-            @RequestParam("imagenFile") MultipartFile imagenFile) {        
-        if (!imagenFile.isEmpty()) {
-            productoService.save(producto);
-            producto.setRutaImagen(
-                    firebaseStorageService.cargaImagen(
-                            imagenFile, 
-                            "producto", 
-                            producto.getIdProducto()));
-        }
-        productoService.save(producto);
-        return "redirect:/producto/listado";
+        return "/pruebas/listado";  
     }
 
-    @GetMapping("/eliminar/{idProducto}")
-    public String productoEliminar(Producto producto) {
-        productoService.delete(producto);
-        return "redirect:/producto/listado";
-    }
-
-    @GetMapping("/modificar/{idProducto}")
-    public String productoModificar(Producto producto, Model model) {
-        producto = productoService.getProducto(producto);
-        model.addAttribute("producto", producto);
-        return "/producto/modifica";
-    }
     
     
 }
--- tienda_1/src/main/java/tienda/Tienda/domain/Producto.java
+++ tienda_1/src/main/java/tienda/Tienda/domain/Producto.java
@@ -7,23 +7,25 @@ import lombok.Data;
 @Data
 @Entity
 @Table(name = "producto")
-public class Producto implements Serializable{
-    
+public class Producto implements Serializable {
+
     private static final long serialVersionUID = 1L;
-    
+
     @Id
-    @GeneratedValue(strategy =GenerationType.IDENTITY)
-    @Column(name="id_producto")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_producto")
     private Long idProducto;
     
-    
-    private Long idCategoria;
     private String descripcion;
     private String detalle;
     private double precio;
     private int existencias;
     private String rutaImagen;
     private boolean activo;
+
+    @ManyToOne​
+    @JoinColumn(name = "id_categoria")
+    Categoria categoria ;
     
 
     public Producto() {
@@ -33,5 +35,5 @@ public class Producto implements Serializable{
         this.descripcion = descripcion;
         this.activo = activo;
     }
-    
+
 }
